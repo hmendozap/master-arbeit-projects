a63fbeaeeb0ca016983c5f2c0df0a0dadc5d8016
hmendozap <heist.mendoza@gmail.com>
2016-03-11 22:48:09 +0100

    Changed fixed CS test file

--- autosk_dev_test/autosk_test_fixedCS.py
+++ autosk_dev_test/autosk_test_fixedCS.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from autosklearn.pipeline.components.classification import add_classifier
-import autosklearn.automl as autosk
+import autosklearn.automl_expanded as autosk
 from component import DeepFeedNet
 
 dataset_dir = '/home/mendozah/workspace/datasets/dataset_728/'
@@ -17,24 +17,23 @@ fixed_cs = '/home/mendozah/workspace/projects/autosk_dev_test/' \
            'tmp/fixed_cs/fix_cs.pcs'
 
 # Create model
-modl = autosk.AutoML(time_left_for_this_task=600, per_run_time_limit=90,
-                     delete_tmp_folder_after_terminate=False,
-                     tmp_dir='tmp/fixed_cs', output_dir='tmp/autosk_out',
-                     log_dir='tmp/autosk_log',
-                     include_estimators=['DeepFeedNet'],
-                     include_preprocessors=['no_preprocessing'],
-                     ensemble_size=0,
-                     ensemble_nbest=0,
-                     initial_configurations_via_metalearning=0,
-                     seed=20,
-                     ml_memory_limit=3072,
-                     metadata_directory=None,
-                     queue=None,
-                     keep_models=True,
-                     debug_mode=False,
-                     resampling_strategy='holdout',
-                     resampling_strategy_arguments=None,
-                     fixed_configuration_space=fixed_cs)
+modl = autosk.ExpAutoML(time_left_for_this_task=600, per_run_time_limit=90,
+                        delete_tmp_folder_after_terminate=False,
+                        tmp_dir='tmp/fixed_cs', output_dir='tmp/autosk_out',
+                        log_dir='tmp/autosk_log',
+                        include_estimators=['DeepFeedNet'],
+                        include_preprocessors=['no_preprocessing'],
+                        ensemble_size=0,
+                        ensemble_nbest=0,
+                        seed=20,
+                        ml_memory_limit=3072,
+                        metadata_directory=None,
+                        queue=None,
+                        keep_models=True,
+                        debug_mode=False,
+                        resampling_strategy='holdout',
+                        resampling_strategy_arguments=None,
+                        fixed_configuration_space=fixed_cs)
 
 modl.fit(X_train, y_train, dataset_name='728_FixCS')
 
