6b49862cbf7ecad3b94447b3bac4ffbdf1fe398c
hmendozap <heist.mendoza@gmail.com>
2016-04-02 14:36:16 +0200

    Modified to test on new autosklearn

--- autosk_dev_test/autosk_test_modified.py
+++ autosk_dev_test/autosk_test_modified.py
@@ -1,31 +1,30 @@
 # -*- encoding: utf-8 -*-
 
 import numpy as np
-from autosklearn.pipeline.components.classification import add_classifier
 import autosklearn.automl as autosk
-from autosklearn.constants import BINARY_CLASSIFICATION
-from component import DeepFeedNet
+import sklearn.metrics
+from autosklearn.constants import BINARY_CLASSIFICATION,\
+    MULTICLASS_CLASSIFICATION
 
 dataset_dir = 
 
 # Load our training data
-X_train = np.load(dataset_dir + 'train.npy')
-y_train = np.load(dataset_dir + 'train_labels.npy')
-
-add_classifier(DeepFeedNet.DeepFeedNet)
+X_train = np.loadtxt(dataset_dir + 'train.data')
+y_train = np.loadtxt(dataset_dir + 'train.solution')
+y_train = np.argmax(y_train, axis=1)
 
 # Create model
-modl = autosk.AutoML(time_left_for_this_task=600, per_run_time_limit=90,
+modl = autosk.AutoML(time_left_for_this_task=1800, per_run_time_limit=600,
                      delete_tmp_folder_after_terminate=False,
                      tmp_dir='tmp/autosk_tmp', output_dir='tmp/autosk_out',
                      log_dir='tmp/autosk_log',
                      include_estimators=['DeepFeedNet'],
-                     include_preprocessors=['NoPreprocessing'],
-                     ensemble_size=0,
-                     ensemble_nbest=0,
+                     include_preprocessors=['no_preprocessing'],
+                     ensemble_size=2,
+                     ensemble_nbest=2,
                      initial_configurations_via_metalearning=0,
                      seed=50,
-                     ml_memory_limit=2048,
+                     ml_memory_limit=4096,
                      metadata_directory=None,
                      queue=None,
                      keep_models=False,
@@ -33,19 +32,13 @@ modl = autosk.AutoML(time_left_for_this_task=600, per_run_time_limit=90,
                      resampling_strategy='holdout',
                      resampling_strategy_arguments=None)
 
-modl.fit(X_train, y_train, dataset_name='728_Testing',
-         task=BINARY_CLASSIFICATION)
+modl.fit(X_train, y_train, dataset_name='evita_Testing',
+         task=MULTICLASS_CLASSIFICATION)
 
-X_test = np.load(dataset_dir + 'test.npy')
-y_test = np.load(dataset_dir + 'test_labels.npy')
+X_test = np.loadtxt(dataset_dir + 'test.data')
+y_test = np.loadtxt(dataset_dir + 'test.solution')
+y_test = np.argmax(y_test, axis=1)
 
-try:
-    tot_score = modl.score(X_test, y_test)
-    print(tot_score)
-
-    y_pred = modl.predict(X_test)
-    # Comparison
-    accuracy = np.count_nonzero(y_test == y_pred)
-    print (float(accuracy) / X_test.shape[0])
-except Exception as E:
-    print("Ups, there is an error: %s" % E)
+print(modl.show_models())
+predictions = modl.predict(X_test)
+print("Accuracy score", sklearn.metrics.accuracy_score(y_test, predictions))
