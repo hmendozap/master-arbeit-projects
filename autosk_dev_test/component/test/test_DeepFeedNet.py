3adace4c3fdb2e95fd43159ec31ba1279e626f9c
hmendozap <heist.mendoza@gmail.com>
2016-05-02 13:11:19 +0200

    Update test component files (cherry picked from commit fdb4c94 - gputrack)

--- autosk_dev_test/component/test/test_DeepFeedNet.py
+++ autosk_dev_test/component/test/test_DeepFeedNet.py
@@ -1,9 +1,6 @@
 import unittest
-
-from component.DeepFeedNet import DeepFeedNet
-from component.ConstrainedFeedNet import ConstrainedFeedNet, AdamConstFeedNet
-from autosklearn.pipeline.util import _test_classifier, _test_classifier_predict_proba
-
+from autosklearn.pipeline.components.classification.DeepFeedNet import DeepFeedNet
+from autosklearn.pipeline.util import _test_classifier
 import sklearn.metrics
 
 
@@ -11,24 +8,38 @@ class NetComponentTest(unittest.TestCase):
     def test_default_configuration(self):
         for i in range(10):
             predictions, targets = _test_classifier(DeepFeedNet, dataset='iris')
-            print sklearn.metrics.accuracy_score(predictions, targets)
-            # self.assertAlmostEqual(0.96,
-            #                       sklearn.metrics.accuracy_score(predictions, targets))
-
-    def test_default_sparse_configuration(self):
-        for i in range(10):
-            predictions, targets = _test_classifier(DeepFeedNet, sparse='True')
-            acc_score = sklearn.metrics.accuracy_score(y_pred=predictions,
-                                                       y_true=targets)
-            # self.assertAlmostEqual(0.3, acc_score, places=2)
-
-    def test_constrained_default_configuration_space(self):
-        for i in range(10):
-            predictions, targets = _test_classifier(AdamConstFeedNet,
-                                                    dataset='iris')
-            acc_score = sklearn.metrics.accuracy_score(y_true=targets,
-                                                       y_pred=predictions)
+            acc_score = sklearn.metrics.accuracy_score(y_pred=predictions, y_true=targets)
             print(acc_score)
+            self.assertAlmostEqual(0.34, acc_score)
+
+    def test_default_configuration_binary(self):
+        """
+        Test of default config feed net in
+        a binary classification problem
+        """
+        for i in range(10):
+            predictions, targets = _test_classifier(DeepFeedNet,
+                                                    make_binary=True)
+            acc_score = sklearn.metrics.accuracy_score(y_pred=predictions, y_true=targets)
+            print(acc_score)
+            self.assertAlmostEqual(0.99, acc_score, places=1)
+
+    def test_default_configuration_sparse(self):
+        for i in range(10):
+            predictions, targets = \
+                _test_classifier(DeepFeedNet, sparse=True)
+            acc_score = sklearn.metrics.accuracy_score(y_pred=predictions, y_true=targets)
+            print(acc_score)
+            self.assertAlmostEqual(.4, acc_score, places=1)
+
+    def test_default_configuration_multilabel(self):
+        for i in range(10):
+            predictions, targets = _test_classifier(DeepFeedNet,
+                                                    make_multilabel=True)
+            self.assertEqual(predictions.shape, (50, 3))
+            ave_precision_score = sklearn.metrics.average_precision_score(targets, predictions)
+            print(ave_precision_score)
+            self.assertAlmostEqual(0.79, ave_precision_score, places=1)
 
     def test_constrained_individual_configspace(self):
         # TODO: Test for fixed cs
--- autosk_dev_test/component/test/test_LinReg.py
+++ autosk_dev_test/component/test/test_LinReg.py
@@ -8,7 +8,7 @@ import sklearn.metrics
 class LinRegComponentTest(unittest.TestCase):
     def test_default_configuration(self):
         for i in range(10):
-            predictions, targets = _test_regressor(LinReg)
-            score_r2 = sklearn.metrics.r2_score(y_true=targets, y_pred=predictions)
-            print(score_r2)
-            # self.assertAlmostEqual(0.1212, score_r2)
+            predictions, targets =_test_regressor(LinReg)
+            R2score = sklearn.metrics.r2_score(y_true=targets, y_pred=predictions)
+            print(R2score)
+            self.assertAlmostEqual(0.1212, R2score, places=1)
